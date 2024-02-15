8bc9999ec2de980eff7438efb364baa6e2c437c3
kimseonha <158234996+kimseonha@users.noreply.github.com>
2024-02-15 14:30:53 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,10 +1,7 @@
 function solution(my_string, n) {
     let answer = '';
     for (let i = 0; i < my_string.length; i++) {
-        for (let j = 0; j < n; j++) {
-            console.log(my_string[i]);
-            answer += my_string[i];
-        }
+            answer += my_string[i].repeat(n);
     }
     return answer;
 }
